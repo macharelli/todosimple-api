772eac6f9623cc948b02fb1ccb19dc99e9f99b0f
macharelli <150154635+macharelli@users.noreply.github.com>
2024-03-05 10:06:57 -0300

    Create User Spring Security

---
+++ src/main/java/com/Macharelli/demo/security/UserSpringSecurity.java
@@ -0,0 +1,54 @@
+package com.Macharelli.demo.security;
+
+import com.Macharelli.demo.models.enums.ProfileEnum;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@Getter
+public class UserSpringSecurity implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority>authorities;
+
+    public UserSpringSecurity(Long id, String username, String password, Set<ProfileEnum> profileEnum) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = profileEnum.stream().map(x-> new SimpleGrantedAuthority(x.getDescription())).collect(Collectors.toList());
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    public boolean hasRole(ProfileEnum profileEnum){
+       return  getAuthorities().contains(new SimpleGrantedAuthority(profileEnum.getDescription()));
+    }
+
+}
